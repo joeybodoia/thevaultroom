9b9a219a02f027783b4f6d3b3704bed06b9e7580
joeybodoia <jbodoia21@cmc.edu>
2025-09-11 17:16:25 -0700

    Fix oversized styling in Bolt.new preview

--- src/App.tsx
+++ src/App.tsx
@@ -111,7 +111,7 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white bolt-scale-fix">
       <Header />
       <main>
         <HeroBanner />
